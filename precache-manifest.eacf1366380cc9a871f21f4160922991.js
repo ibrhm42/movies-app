0737825a10d86df657da9597dc87ef036e829386
Ibrahim Khalil <78839403+ibrahimkhalil92@users.noreply.github.com>
2025-11-06 20:39:21 +0500

    Deploy

---
+++ precache-manifest.eacf1366380cc9a871f21f4160922991.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b30ca9f5c3b7605ba115b839907f8af",
+    "url": "/imdb/index.html"
+  },
+  {
+    "revision": "025523a15c9d1b38de21",
+    "url": "/imdb/static/css/main.25bb5bfc.chunk.css"
+  },
+  {
+    "revision": "3bb3e486e69e65ae77b9",
+    "url": "/imdb/static/js/2.286a7748.chunk.js"
+  },
+  {
+    "revision": "54010ba5ae5fd5a190fa7b6c659db4ab",
+    "url": "/imdb/static/js/2.286a7748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "025523a15c9d1b38de21",
+    "url": "/imdb/static/js/main.1ea831ea.chunk.js"
+  },
+  {
+    "revision": "f9ef74a7d2c22b9f4fba",
+    "url": "/imdb/static/js/runtime-main.4044208f.js"
+  },
+  {
+    "revision": "fa275866a285661bf4cc157bad24a6fb",
+    "url": "/imdb/static/media/tom-cruise.fa275866.jpg"
+  }
+]);
